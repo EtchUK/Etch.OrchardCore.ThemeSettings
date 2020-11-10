050dc6089dafb4f1701c9438a04fc72fa95a9f68
Peter Keating <pete.keating@etchuk.com>
2020-11-10 23:46:03 +0000

    Add menu option to theme settings under 'Design'
    Fixes #1.

---
+++ AdminMenu.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Localization;
+using OrchardCore.Navigation;
+using System;
+using System.Threading.Tasks;
+
+namespace Etch.OrchardCore.ThemeSettings
+{
+    public class AdminMenu : INavigationProvider
+    {
+        private readonly IStringLocalizer S;
+
+        public AdminMenu(IStringLocalizer<AdminMenu> localizer)
+        {
+            S = localizer;
+        }
+
+        public Task BuildNavigationAsync(string name, NavigationBuilder builder)
+        {
+            if (!string.Equals(name, "admin", StringComparison.OrdinalIgnoreCase))
+            {
+                return Task.CompletedTask;
+            }
+
+            builder
+                .Add(S["Design"], NavigationConstants.AdminMenuDesignPosition, design => design
+                    .AddClass("themes").Id("themes")
+                    .Add(S["Theme Settings"], "ThemeSettings", installed => installed
+                        .Action("Index", "Admin", new { area = "OrchardCore.Settings", groupId = "ThemeSettings" })
+                        .LocalNav()
+                    )
+                );
+
+            return Task.CompletedTask;
+        }
+    }
+}
