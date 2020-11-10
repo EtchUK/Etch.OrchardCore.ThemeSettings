5b7662550fb0103e9daa3cafdf0e520a475318b8
Peter Keating <pete.keating@etchuk.com>
2020-11-10 23:57:40 +0000

    Bump to v0.1.1

--- Manifest.cs
+++ Manifest.cs
@@ -6,6 +6,6 @@ using OrchardCore.Modules.Manifest;
     Dependencies = new[] { "OrchardCore.ContentFields", "OrchardCore.CustomSettings" },
     Description = "Customise visual appearance of site.",
     Name = "Theme Settings",
-    Version = "0.1.0",
+    Version = "0.1.1",
     Website = "https://etchuk.com"
 )]
